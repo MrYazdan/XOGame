8cb05a150fe346fa3644d6d8e05fd397d4eb381f
Mr Yazdan <mrrezayazdani@yahoo.com>
2021-04-29 00:13:32 +0430

    add Developer and delete welcome massage

--- functions.py
+++ functions.py
@@ -7,8 +7,7 @@ def clear():
 
 def welcome_page():
     termcolor.cprint("-" * 55, color="cyan")
-    termcolor.cprint(" Welcome To :", color="cyan")
     termcolor.cprint(pyfiglet.figlet_format(" |= XO Game"), color="green", end=" ")
     print("Source : https://github.com/MrYazdan/XOGame_Maktab52")
-    print(" Version : 1.0.0")
+    print(" Version : 1.0.0  -  Developer : Yazdan")
     termcolor.cprint("-" * 55, color="cyan")
