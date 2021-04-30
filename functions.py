9c5c8195fa0dd399581e6f9e44afc92e67c8f404
Mr Yazdan <mrrezayazdani@yahoo.com>
2021-05-01 01:19:43 +0430

    Optimize welcome page functions

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ def welcome_page(string: str, count_ward: int):
     clear()
     termcolor.cprint("=" * 56, color="cyan")
     termcolor.cprint(pyfiglet.figlet_format(" |= XO Game"), color="green", end=" ")
-    print("Source : https://github.com/MrYazdan/XOGame_Maktab52")
+    print(" Source : https://github.com/MrYazdan/XOGame_Maktab52")
     print(" Version : 1.0.0  -  Developer : Yazdan\n")
 
     if not (string == "" and count_ward == 0):
@@ -26,29 +26,24 @@ def iscommand(_from: str, cmd: str) -> bool:
 
 def command_error(_from: str) -> None:
     if _from == "main":
-        welcome_page()
-        termcolor.cprint(f"{'=' * 20} COMMAND  ERROR {'=' * 20}", color="cyan")
-        print(" Print Your Command Was Not Defined! :(")
-        _tmp = input(" Press Enter To Load Main Again ~")
+        welcome_page(" COMMAND  ERROR ", 20)
+        _tmp = input(" Print Your Command Was Not Defined! :(\n Press Enter To Load Main Again ~")
 
 
 def exit_page(_from: str) -> None:
     if _from == "main":
-        welcome_page()
-        termcolor.cprint(f"{'=' * 25} EXIT {'=' * 25}", color="cyan")
-        print(" What Are You Doing !?")
-        _tmp = input(" Are you sure to exit the game ? ".title() + "[y/N] : ")
+        welcome_page(" EXIT ", 25)
+        _tmp = input(" What Are You Doing !?\n Are you sure to exit the game ? ".title() + "[y/N] : ")
 
         if _tmp.lower() == "y":
-            welcome_page()
+            welcome_page("", 0)
             termcolor.cprint(pyfiglet.figlet_format(" Goodbye !", font="small"), color="red")
             exit()
 
 
 def setting_panel(_from: str) -> None:
     if _from == "main":
-        welcome_page()
-        termcolor.cprint(f"{'=' * 23} SETTINGS {'=' * 23}", color="cyan")
+        welcome_page(" SETTINGS ", 23)
 
         print(
             f"""                    1. Round Count : {Setting.ROUND_COUNT}        
@@ -62,20 +57,17 @@ def setting_panel(_from: str) -> None:
 
         if cmd == "1":
             while True:
-                welcome_page()
-                termcolor.cprint(f"{'=' * 14} SETTING -> SET ROUND COUNT {'=' * 14}", color="cyan")
+                welcome_page(" SETTING -> SET ROUND COUNT ", 14)
                 _tmp = input(" Enter Number Of Round To Play Game [1 / 9] : ")
 
                 if 1 <= int(_tmp) <= 9:
                     Setting.ROUND_COUNT = int(_tmp)
-                    welcome_page()
-                    termcolor.cprint(f"{'=' * 14} SETTING -> SET ROUND COUNT {'=' * 14}", color="cyan")
+                    welcome_page(" SETTING -> SET ROUND COUNT ", 14)
                     _tmp = input(" New Round Count Saved !\n Press Enter To Back Main ~ ")
                     break
 
                 else:
-                    welcome_page()
-                    termcolor.cprint(f"{'=' * 14} SETTING -> SET ROUND COUNT {'=' * 14}", color="cyan")
+                    welcome_page(" SETTING -> SET ROUND COUNT ", 14)
                     _tmp = input(" Please Enter True Value !\n Press Enter To Continue ~ ")
                     continue
 
@@ -84,19 +76,16 @@ def start_game_panel():
     tmp_list = []
 
     for i in range(2):
-        welcome_page()
-        termcolor.cprint(f"{'=' * 19} INITIALIZE  GAME {'=' * 19}", color="cyan")
+        welcome_page(" INITIALIZE  GAME ", 19)
         tmp_list.append(input(f" Please Enter Name Of Player {i + 1} : "))
 
     Setting.PL1_NAME = tmp_list[0]
     Setting.PL2_NAME = tmp_list[1]
 
-    welcome_page()
-    termcolor.cprint(f"{'=' * 19} INITIALIZE  GAME {'=' * 19}", color="cyan")
-    print()
-    termcolor.cprint(f" Player 1 => {Setting.PL1_NAME} With Sign : 'X'", color="red")
-    termcolor.cprint(f" Player 2 => {Setting.PL2_NAME} With Sign : 'O'", color="blue")
-    print()
+    welcome_page(" INITIALIZE  GAME ", 19)
+    termcolor.cprint(f"\n Player 1 => {Setting.PL1_NAME} With Sign : 'X'", color="red")
+    termcolor.cprint(f" Player 2 => {Setting.PL2_NAME} With Sign : 'O'\n", color="blue")
+
     termcolor.cprint(f"{'=' * 56}", color="cyan")
     _tmp = input(" Press Enter To Play Game ~ ")
 
@@ -106,17 +95,14 @@ def normalize_cell_no(data: str) -> int:
         if 0 < int(data) < 10:
             return int(data)
 
-    welcome_page()
-    termcolor.cprint(f"{'=' * 15} PLAY -> GET TRUE VALUES FOR MARK {'=' * 15}", color="cyan")
-    print()
-    _data = input("Enter True Value For Select The Mark [1 ~ 9] : ")
+    welcome_page(" PLAY -> GET TRUE VALUES FOR MARK ", 15)
+
+    _data = input("\n Enter True Value For Select The Mark [1 ~ 9] : ")
     return normalize_cell_no(_data)
 
 
 def show_last_result():
-    welcome_page()
-    termcolor.cprint(f"{'=' * 21} SHOW  RESULT {'=' * 21}", color="cyan")
-    print()
-    termcolor.cprint(f" >> {Setting.LAST_RESULT}", color="cyan")
-    print()
+    welcome_page(" SHOW  RESULT ", 21)
+
+    termcolor.cprint(f"\n >> {Setting.LAST_RESULT} \n", color="cyan")
     _tmp = input(" Press Enter To Back Main ~ ")
